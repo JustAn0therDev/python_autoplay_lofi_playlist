4c6cd66d84f603c98132b25ec590cacebb60830f
Ruan Montelo <ruanmontelo@gmail.com>
2020-05-04 22:43:09 -0300

    Creating a class to keep all of the logic of managing the player in one place

---
+++ Spotipy_Player.py
@@ -0,0 +1,34 @@
+# make the playlist random as well
+import spotipy
+import spotipy.util as util
+import time
+import spotify_credentials
+
+class Spotipy_Player:
+    scope = 'user-modify-playback-state'
+    scope_two = 'user-read-playback-state'
+    context_uri_lofi_playlist = 'spotify:playlist:74sUjcvpGfdOvCHvgzNEDO'
+    token = ''
+    first_available_device_id = ''
+    spotipy_instance = ''
+
+    def get_token(self) -> None:
+        self.token = util.prompt_for_user_token(
+        spotify_credentials.USERNAME, 
+        f'{self.scope}%20{self.scope_two}',
+        spotify_credentials.CLIENT_ID, 
+        spotify_credentials.CLIENT_SECRET,
+        spotify_credentials.REDIRECT_URI)
+
+    def initalize_spotipy(self) -> None:
+        self.spotipy_instance = spotipy.Spotify(self.token)
+
+    def get_first_available_device(self) -> None:
+        avaliable_devices = self.spotipy_instance.devices()
+        self.first_available_device_id = avaliable_devices['devices'][0]['id']
+
+    def start_playback(self) -> None:
+        self.spotipy_instance.start_playback(self.first_available_device_id, self.context_uri_lofi_playlist)
+
+    def repeat_playback(self, state) -> None:
+        self.spotipy_instance.repeat(state, self.first_available_device_id)
--- main.py
+++ main.py
@@ -1,21 +1,12 @@
-import spotipy
-import spotipy.util as util
-import time
-import spotify_credentials
+import Spotipy_Player
 
-scope = 'user-modify-playback-state'
-scope_two = 'user-read-playback-state'
-context_uri_lofi_playlist = 'spotify:playlist:74sUjcvpGfdOvCHvgzNEDO'
-token = util.prompt_for_user_token(spotify_credentials.username, f'{scope}%20{scope_two}',spotify_credentials.CLIENT_ID, spotify_credentials.CLIENT_SECRET, 'http://localhost:3333/')
+spotipy_player = Spotipy_Player.Spotipy_Player()
 
 try:
-    sp = spotipy.Spotify(auth=token)
-
-    available_devices = sp.devices()
-    first_available_device_id = available_devices['devices'][0]['id']
-
-    sp.start_playback(first_available_device_id, context_uri_lofi_playlist)
-
-    sp.repeat('context', first_available_device_id)
+    spotipy_player.get_token()
+    spotipy_player.initalize_spotipy()
+    spotipy_player.get_first_available_device()
+    spotipy_player.start_playback()
+    spotipy_player.repeat_playback('context')
 except Exception as error_message:
     print(f'The spotify request could not be made because of the following error: {error_message}')
